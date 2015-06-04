a7f190b08f493c50115638d9b1b9f185b00461e4
Leo Lam <130.leo@gmail.com>
2015-06-03 21:20:18 -0400

    Update departure

--- a1main.cpp
+++ a1main.cpp
@@ -9,6 +9,7 @@ using namespace std;
  */
 double average_queue_packets = 0; // Average number of packets in the queue
 double average_sojourn_time = 0; // Queueing delay + Service time
+double idle_time = 0;
 double proportion_idle = 0; // Proportion of ticks the server is idle
 double probability_packet_loss = 0; // Probability of packet loss (only relevant for M/D/1/K)
 
@@ -19,6 +20,7 @@ unsigned long tick_length = 0; // Simulation length (in ticks)
 double avg_number_packets = 0; // Average packets per second to generate
 unsigned long packet_length = 0; // Length of packets to send (in bits)
 double transmission_rate = 0; // Transmission rate (bits per second)
+double service_time = 0;
 unsigned long queue_size = 0; // Queue capacity (for M/D/1/K queue)
 
 queue<unsigned long> Queue;
@@ -46,19 +48,19 @@ void arrival(int t)
 	}
 }
 
+/* Check the queue for the packet, if head of the queue is empty,
+return 0 else if the queue is non-empty delete the packet from the
+queue after an elapse of the deterministic service time. */
 int departure(int t)
 {
-	/* Check the queue for the packet, if head of the queue is empty,
-	return 0 else if the queue is non-empty delete the packet from the
-	queue after an elapse of the deterministic service time. */
 	if (Queue.empty()) {
-		idleTime++;
+		idle_time++;
 		return 0;
 	} else {
 		t_departure--;
 
 		if (t_departure < 0) {
-			t_departure = serviceTime;
+			t_departure = service_time;
 			Queue.pop();
 		}
 
@@ -124,6 +126,8 @@ void main()
 	cout << "Transmission rate: ";
 	cin >> transmission_rate;
 	cout << endl;
+
+	service_time = packet_length / transmission_rate;
 
 	// Start off by determining when to send the first packet
 	update_next_tick();
